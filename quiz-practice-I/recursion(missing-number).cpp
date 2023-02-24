ed31cb1ee2a7cab4c0559163bf8ffc0587c31374
MaleekNoob <maleekhussainali456@gmail.com>
2023-02-24 20:37:06 +0500

    commit 12

--- quiz-practice-I/recursion(missing-number).cpp
+++ quiz-practice-I/recursion(missing-number).cpp
@@ -1,21 +1,51 @@
 #include <iostream>
 using namespace std;
 
-int missingNumber(int array[], int size) {
-    static int i;
-    if (i < (size - 1)) {
-        
-        i++;
-        missingNumber(array, size);
+bool compareArrayNumber(int array[], int index, int size) {
+    
+    static int j;
+    if (j < size) {
+
+        if (index == array[j]) {
+            j = 0;
+            return true;
+        }
+
+        j++;
+        return (false || compareArrayNumber(array, index, size));
+    }
+    else {
+        j = 0;
+        return false;
     }
 
-    return NULL;
+}
+
+int missingNumber(int array[], int size) {
+    
+    static int i;
+
+    if (i == 0)
+        i = 1;
+
+    if (i <= size) {
+        
+        if (!compareArrayNumber(array, i, size)) {
+            return i;
+        }
+
+        i++;
+        return missingNumber(array, size);
+    }
+    else {
+        return -1;
+    }
 }
 
 int main() {
 
-    int N = 5;
-    int array[] = {1, 2, 3, 5};
+    int N = 15;
+    int array[] = {6, 9, 1, 3, 2, 8, 4, 7, 5, 10, 15, 13, 12, 14};
 
     cout << missingNumber(array, N);
 
