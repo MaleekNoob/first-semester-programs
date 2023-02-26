b8d05f1e2be498d476c4f5edf4b67d170e295ebb
MaleekNoob <maleekhussainali456@gmail.com>
2023-02-26 19:12:13 +0500

    commit 16

---
+++ quiz-practice-I/classes(pangy).cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class A {
+    private:
+    int a;
+    int &cRef = a;
+
+    public:
+    A() {
+        a = 0;
+    }
+
+    A(int &A): a(A) {}
+
+    int getA() {
+        return a;
+    }
+
+    void setA(int A) {
+        a = A;
+    }
+
+    void display() {
+        cout << "a: " << a << endl;
+    }
+};
+
+int main() {
+
+    int b = 10;
+    A a(b);
+    A &aRef = a;
+    aRef.display();
+
+
+    return 0;
+}
