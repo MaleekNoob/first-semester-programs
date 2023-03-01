9789384408c894a9d0edb3d34d1d83bff55d4fa2
MaleekNoob <maleekhussainali456@gmail.com>
2023-03-01 16:08:27 +0500

    commit 23

--- quiz-practice-I/lab-question-3.cpp
+++ quiz-practice-I/lab-question-3.cpp
@@ -3,14 +3,49 @@ using namespace std;
 
 class Sphere {
     private: 
-    const int PI = 3.14159;
-    int radius;
+    const double PI = 3.14159;
+    double radius;
 
     public:
     Sphere() {
         radius = 0;
     }
 
+    Sphere(double radius) {
+        this->radius = radius;
+    }
 
+    void SetRadius(double radius) {
+        this->radius = radius;
+    }
+
+    double getRadius() {
+        return radius;
+    }
+
+    double getDiameter() {
+        return (radius * 2);
+    }
+
+    double getArea() {
+        return (4 * PI * radius * radius);
+    }
+
+    double GetVolume() {
+        return ((4.0 / 3.0) * PI * radius * radius * radius);
+    }   
+
+    double GetCircumference() {
+        return (2 * PI * radius);
+    }
 
 };
+
+int main() {
+
+    double r;
+    Sphere s;
+    cout << "\nEnter radius: ";
+    cin >> r;
+
+}
