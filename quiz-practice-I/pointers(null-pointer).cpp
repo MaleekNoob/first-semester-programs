b367907728727a21c8becbddd0beb7c7251e73a4
MaleekNoob <maleekhussainali456@gmail.com>
2023-03-05 22:55:19 +0500

    commit 34

---
+++ quiz-practice-I/pointers(null-pointer).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int num = 10;
+    void *p;
+    int *p1 = &num;
+    (int)p = p1;
+    cout << *p << endl;
+    cout << *p1 << endl;
+    // p1 = NULL;
+    return 0;
+}
