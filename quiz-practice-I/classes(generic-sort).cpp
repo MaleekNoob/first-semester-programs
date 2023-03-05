1355418e70ee57c128a043e718b669c9d8db0a33
MaleekNoob <maleekhussainali456@gmail.com>
2023-03-05 14:45:58 +0500

    commit 30

--- quiz-practice-I/classes(generic-sort).cpp
+++ quiz-practice-I/classes(generic-sort).cpp
@@ -2,7 +2,30 @@
 using namespace std;
 
 
+class sort {
+    private:
+    int* intArr = new int;
+    float* floatArr = new float;
+    string* stringArr = new string;
+
+    public: 
+    sort() {}
+    sort(int* a, float* b, string* c):intArr(a), floatArr(b), stringArr(c) {}
+    void sortArray(int size, int type);
+
+};
+
 int main() {
+
+    int n, q;
+
+    cout << "\nEnter size: ";
+    cin >> n;
+    cout << "\nEnter type: ";
+    cin >> q;
+
+    sort element;
+    element.sortArray(n , q);
 
     return 0;
 }
