e749a13f5cf7dca7e2f7ca4abe50a1ae2874a3c5
MaleekNoob <maleekhussainali456@gmail.com>
2023-03-08 15:14:58 +0500

    commit 38

---
+++ labtask7/task3.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Account {
+    private:
+    long account_number;
+    double balance;
+    static int base;
+
+    public:
+    Account() : account_number(base) {
+        this->balance = 0;
+        base += 100000;
+        account_number = base;
+    }
+
+    Account(double balance) {
+
+        if (balance < 0) {
+            cout << "Balance cannot be negative" << endl;
+            exit(0);
+        }
+
+        this->balance = balance;
+        base += 100000;
+        account_number = base;
+    }
+
+    void checkBalance () const {
+        cout << "Balance: " << balance << endl;
+    }
+
+    void deposit (double amount) {
+        if (amount < 0) {
+            cout << "Amount cannot be negative" << endl;
+            exit(0);
+        }
+
+        balance += amount;
+    }
+
+    void withdraw (double amount) {
+        if (amount < 0) {
+            cout << "Amount cannot be negative" << endl;
+            exit(0);
+        }
+
+        if (amount > balance) {
+            cout << "Insufficient balance" << endl;
+            exit(0);
+        }
+
+        balance -= amount;
+    }
+
+    ~Account() {
+        cout << "Account " << account_number << " destroyed and transferred to authorized perosn" << endl;
+    }
+
+};
+
+int main() {
+
+
+    return 0;
+}
